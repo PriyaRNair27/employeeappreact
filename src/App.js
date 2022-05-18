bd500f156d861c8e92119be426df25d059f3a005
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-19 02:30:53 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addemp from './Components/Addemp';
 import Search from './Components/Search';
+import Header from './Components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-<Addemp/>
-<Search/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Addemp/>}/>
+        <Route path="/search" exact element={<Search/>}/>
+        <Route path="/" exact element/>
+        </Routes></BrowserRouter>
+    
 
 
     </div>
