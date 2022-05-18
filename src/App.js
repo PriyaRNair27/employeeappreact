0cf525f1cbc6fa36a20eb2b270161905fefccbe4
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-19 02:17:23 +0530

    search

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addemp from './Components/Addemp';
+import Search from './Components/Search';
 
 function App() {
   return (
     <div>
 <Addemp/>
+<Search/>
 
 
     </div>
